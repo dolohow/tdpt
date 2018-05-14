5f3355c7477e378da75bf4bdad4ab282b220ed90
Łukasz Żarnowiecki <dolohow@outlook.com>
2018-05-14 15:36:37 +0200

    Flake8ify

--- tdpt.py
+++ tdpt.py
@@ -13,6 +13,7 @@ TRANSMISSION = transmissionrpc.Client(CONFIG['Transmission']['host'],
                                       port=CONFIG['Transmission']['port'])
 BOT = telegram.Bot(CONFIG['Telegram']['bot_token'])
 
+
 def format_speed(speed):
     speed = float(speed)
     if speed < 1000000:
@@ -41,27 +42,33 @@ Peers:    {}
     def create(self):
         try:
             print('Creating new download message')
-            self.message_id = self.bot.sendMessage(self.chat_id, self._get_new_text(),
-                                                   parse_mode='markdown').message_id
+            self.message_id = self.bot.sendMessage(
+                self.chat_id,
+                self._get_new_text(),
+                parse_mode='markdown').message_id
         except telegram.error.TimedOut:
             print("Timeout")
 
     def update(self):
         self.torrent = TRANSMISSION.get_torrent(self.torrent.id)
         try:
-            self.bot.editMessageText(self._get_new_text(), chat_id=self.chat_id,
-                                     message_id=self.message_id, parse_mode='markdown')
+            self.bot.editMessageText(self._get_new_text(),
+                                     chat_id=self.chat_id,
+                                     message_id=self.message_id,
+                                     parse_mode='markdown')
         except telegram.error.BadRequest as bad_request:
             print(bad_request)
         except telegram.error.TimedOut:
             print("Timeout")
 
     def delete(self):
-        self.bot.deleteMessage(chat_id=self.chat_id, message_id=self.message_id)
+        self.bot.deleteMessage(chat_id=self.chat_id,
+                               message_id=self.message_id)
 
     def _get_new_text(self):
         return self.text.format(self.torrent.name, self.torrent.percentDone,
-                                format_speed(self.torrent.rateDownload), self.torrent.format_eta(),
+                                format_speed(self.torrent.rateDownload),
+                                self.torrent.format_eta(),
                                 self.torrent.peersConnected)
 
 
@@ -72,16 +79,21 @@ class TimeCounter:
     def __init__(self):
         self.counter = 0
         self.lock = threading.Lock()
+
     def next(self):
         with self.lock:
             self.counter += 1
+
     def prev(self):
         with self.lock:
             self.counter -= 1
+
     def get_time(self):
         return self.messages_per_second*self.counter+1
 
+
 def handle_torrent(torrent, time_counter, torrents_tracked):
+
     def cleanup(text):
         print(text)
         message.delete()
@@ -103,20 +115,27 @@ def handle_torrent(torrent, time_counter, torrents_tracked):
             if not CONFIG.get('General', 'call_on_finish', fallback=None):
                 return
             subprocess.Popen('notification', env={
-                'TORRENT_NAME': message.torrent.name, 'LC_ALL': 'en_GB.UTF-8', 'LANG': 'en_GB.UTF-8'
+                'TORRENT_NAME': message.torrent.name,
+                'LC_ALL': 'en_GB.UTF-8',
+                'LANG': 'en_GB.UTF-8',
             })
+
 
 def main():
     torrents_tracked = set()
     time_counter = TimeCounter()
     while True:
         for torrent in TRANSMISSION.get_torrents():
-            if torrent.id not in torrents_tracked and torrent.status == 'downloading':
+            if (torrent.id not in torrents_tracked and
+                    torrent.status == 'downloading'):
                 torrents_tracked.add(torrent.id)
                 threading.Thread(target=handle_torrent,
-                                 args=(torrent, time_counter, torrents_tracked)).start()
+                                 args=(torrent,
+                                       time_counter,
+                                       torrents_tracked)).start()
 
         time.sleep(10)
+
 
 if __name__ == '__main__':
     main()
